2ec9627971bf23fc36de2fa35d95466b3214d351
Valentyn Ponomarenko <bootloader@list.ru>
2015-04-01 23:26:43 -0500

    Lesson 8: Добавил реализацию графа и результаты теоретического теста №8

---
+++ Graph/GraphTests.cs
@@ -0,0 +1,311 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Graph
+{
+    public class GraphTests
+    {
+        private readonly Graph _graph;
+
+        public GraphTests()
+        {
+            _graph = new Graph();
+        }
+
+        private void createGraph1()
+        {
+            _graph._graph.Add("s", new[] { "a", "b" });
+            _graph._graph.Add("a", new[] { "s", "c", "d" });
+            _graph._graph.Add("b", new[] { "s", "d" });
+            _graph._graph.Add("c", new[] { "a", "d", "e" });
+            _graph._graph.Add("d", new[] { "a", "b", "c", "e" });
+            _graph._graph.Add("e", new[] { "c", "d" });
+        }
+        private void createGraph2()
+        {
+            _graph._graph.Add("1", new[] { "2"});
+            _graph._graph.Add("2", new[] { "3", "5"});
+            _graph._graph.Add("3", new[] { "2", "4", "5" });
+            _graph._graph.Add("4", new[] { "3", "5", "9" });
+            _graph._graph.Add("5", new[] { "2", "3", "4", "6", "7" });
+            _graph._graph.Add("6", new[] { "5", "7", "8" });
+            _graph._graph.Add("7", new[] { "5", "6", "9" });
+            _graph._graph.Add("8", new[] { "6", "9" });
+            _graph._graph.Add("9", new[] { "4", "7", "8" });
+        }
+
+        private void createGraph2_with_Connected_Components_3()
+        {
+            _graph._graph.Add("1", new[] { "2", "3" });
+            _graph._graph.Add("2", new[] { "1", "3" });
+            _graph._graph.Add("3", new[] { "1", "2" });
+
+            _graph._graph.Add("4", new[] { "5" });
+            _graph._graph.Add("5", new[] { "4" });
+
+            _graph._graph.Add("6", new[] { "7", "8" });
+            _graph._graph.Add("7", new[] { "6", "9" });
+            _graph._graph.Add("8", new[] { "6", "9" });
+            _graph._graph.Add("9", new[] { "7", "8" });
+        }
+        private void createGraph2_with_Connected_Components_4()
+        {
+            _graph._graph.Add("1", new[] { "2", "5" });
+            _graph._graph.Add("2", new[] { "1", "3" });
+            _graph._graph.Add("3", new[] { "2", "4" });
+            _graph._graph.Add("4", new[] { "3", "5" });
+            _graph._graph.Add("5", new[] { "1", "4" });
+
+            _graph._graph.Add("6", new[] { "7", "8" });
+            _graph._graph.Add("7", new[] { "6", "8" });
+            _graph._graph.Add("8", new[] { "6", "7" });
+
+            _graph._graph.Add("9", new[] { "10" });
+            _graph._graph.Add("10", new[] { "9" });
+
+            _graph._graph.Add("11", new[] { "12", "14" });
+            _graph._graph.Add("12", new[] { "11", "13" });
+            _graph._graph.Add("13", new[] { "12", "14" });
+            _graph._graph.Add("14", new[] { "11", "13" });
+
+        }
+
+        private void createGraph1_OrientedGraph()
+        {
+            _graph._graph.Add("Белье", new[] { "Носки", "Штаны", "Рубашка" });
+            _graph._graph.Add("Носки", new[] { "Ботинки"});
+            _graph._graph.Add("Штаны", new[] { "Ботинки" });           
+            _graph._graph.Add("Ботинки", new List<string>() );
+            _graph._graph.Add("Рубашка", new[] { "Куртка" });
+            _graph._graph.Add("Куртка", new List<string>());
+        }
+        private void createGraph2_OrientedGraph_with_Connected_Components_4()
+        {
+            _graph._graph.Add("a", new[] { "b" });
+            _graph._graph.Add("b", new[] { "c", "e", "f" });
+            _graph._graph.Add("c", new[] { "d", "g" });
+            _graph._graph.Add("d", new[] { "c", "h" });
+            _graph._graph.Add("e", new[] { "a", "f" });
+            _graph._graph.Add("f", new[] { "g" });
+            _graph._graph.Add("g", new[] { "f", "h" });
+            _graph._graph.Add("h", new List<string>());
+        }
+
+        [Theory]
+        [InlineData("s", 1)]
+        [InlineData("a", 2)]
+        [InlineData("b", 3)]
+        [InlineData("c", 4)]
+        [InlineData("d", 5)]
+        [InlineData("e", 6)]
+        public void Should_Add_correct_number_to_Vertex_when_make_BFS(string vertex, int value)
+        {
+            //Stub
+            createGraph1();
+
+            //Test
+            _graph.BreadthFirstSearch();
+
+            _graph._bfs.ContainsKey(vertex).Should().BeTrue();
+            _graph._bfs[vertex].Should().Be(value);
+
+        }
+
+        [Theory]
+        [InlineData("s", 0)]
+        [InlineData("a", 1)]
+        [InlineData("b", 1)]
+        [InlineData("c", 2)]
+        [InlineData("d", 2)]
+        [InlineData("e", 3)]
+        public void Should_calculate_correct_path_between_Vertexes_when_make_BFS(string vertex, int value)
+        {
+            //Stub
+            createGraph1();
+
+            //Test
+            _graph.BreadthFirstSearch();
+
+             _graph._d.ContainsKey(vertex).Should().BeTrue();
+            _graph._d[vertex].Should().Be(value);
+        }
+
+        [Theory]
+        //[InlineData("s", 0)]
+        [InlineData("a", 1, "as")]
+        [InlineData("b", 1, "bs")]
+        [InlineData("c", 2, "cas")]
+        [InlineData("d", 2, "das")]
+        //[InlineData("d", 2, "dbs")] -- Failed
+        [InlineData("e", 3, "ecas")]
+        //[InlineData("e", 3, "edbs")] -- Failed
+        //[InlineData("e", 3, "edas")] -- Failed
+        public void Should_create_correct_path_between_Vertexes_when_make_BFS(string vertex, int value, string path)
+        {
+            //Stub
+            createGraph1();
+
+            //Test
+            _graph.BreadthFirstSearch();
+
+            _graph._p.ContainsKey(vertex).Should().BeTrue();
+            var p = vertex;
+            
+            var v = vertex;
+            while (_graph._p.ContainsKey(v))
+            {
+                v = _graph._p[v];
+                p += v;
+            }
+            _graph._d[vertex].Should().Be(value);
+            p.Should().Be(path);
+
+        }
+
+        [Fact]
+        public void Should_return_corect_number_connected_components_3()
+        {
+            //Stub
+            createGraph2_with_Connected_Components_3();
+
+            //Test
+            var result = _graph.FindConnectedComponents();
+
+            result.Should().Be(3);
+        }
+
+        [Fact]
+        public void Should_return_corect_number_connected_components_4()
+        {
+            //Stub
+            createGraph2_with_Connected_Components_4();
+
+            //Test
+            var result = _graph.FindConnectedComponents();
+
+            result.Should().Be(4);
+        }
+
+
+        [Theory]
+        [InlineData("s", 1)]
+        [InlineData("a", 2)]
+        [InlineData("c", 3)]
+        [InlineData("d", 4)]
+        [InlineData("b", 5)]
+        [InlineData("e", 6)]
+        public void Should_Add_correct_number_to_Vertex_when_make_DFSR(string vertex, int value)
+        {
+            //Stub
+            createGraph1();
+
+            //Test
+            _graph.DepthFirstSearch1();
+
+            _graph._dfs.ContainsKey(vertex).Should().BeTrue();
+            _graph._dfs[vertex].Should().Be(value);
+
+        }
+
+
+        [Theory]
+        [InlineData("s", 1)]
+        [InlineData("a", 2)]
+        [InlineData("c", 3)]
+        [InlineData("d", 4)]
+        [InlineData("b", 5)]
+        [InlineData("e", 6)]
+        public void Should_Add_correct_number_to_Vertex_when_make_DFS_1(string vertex, int value)
+        {
+            //Stub
+            createGraph1();
+
+            //Test
+            _graph.DepthFirstSearch2();
+
+            _graph._dfs.ContainsKey(vertex).Should().BeTrue();
+            _graph._dfs[vertex].Should().Be(value, 
+                String.Format("Для вершины:{0} ожидалось значение:{1}, а найдено:{2}", 
+                            vertex, 
+                            value, 
+                            _graph._dfs[vertex]));
+
+        }
+
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("2", 2)]
+        [InlineData("3", 3)]
+        [InlineData("4", 4)]
+        [InlineData("5", 5)]
+        [InlineData("6", 6)]
+        [InlineData("7", 7)]
+        [InlineData("8", 9)]
+        [InlineData("9", 8)]
+        public void Should_Add_correct_number_to_Vertex_when_make_DFS_2(string vertex, int value)
+        {
+            //Stub
+            createGraph2();
+
+            //Test
+            _graph.DepthFirstSearch2();
+
+            _graph._dfs.ContainsKey(vertex).Should().BeTrue();
+            _graph._dfs[vertex].Should().Be(value,
+                String.Format("Для вершины:{0} ожидалось значение:{1}, а найдено:{2}",
+                            vertex,
+                            value,
+                            _graph._dfs[vertex]));
+
+        }
+
+        
+        [Theory]
+        [InlineData("Ботинки", 6)]
+        [InlineData("Носки", 5)]
+        [InlineData("Штаны", 4)]
+        [InlineData("Рубашка", 2)]
+        [InlineData("Куртка", 3)]
+        [InlineData("Белье", 1)] 
+        public void Should_Add_correct_sortNumber_when_make_TopologicalSort(string vertex, int value)
+        {
+            //Stub
+            createGraph1_OrientedGraph();
+
+            //Test
+            _graph.TopologicalSort();
+
+            _graph._ts.ContainsKey(vertex).Should().BeTrue();
+            _graph._ts[vertex].Should().Be(value);
+        }
+
+
+        [Theory]
+        [InlineData("h")]
+        [InlineData("aeb")]
+        [InlineData("cd")]
+        [InlineData("fg")]
+        public void Should_find_strong_components_for_Topological_graph(string strongComponent)
+        {
+            //Stub
+            createGraph2_OrientedGraph_with_Connected_Components_4();
+
+            //Test
+            var result = _graph.StrongConnectedComponents();
+
+            var isStrongComponentFound = false;
+            foreach (var componentList in result)
+            {
+                var component = String.Concat(componentList);
+
+                isStrongComponentFound = component == strongComponent;
+                if (isStrongComponentFound) break;
+            }
+
+            isStrongComponentFound.Should().BeTrue();
+
+        }
+    }
+}
--- Graph/Program.cs
+++ Graph/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Graph
 {
@@ -10,11 +10,373 @@ namespace Graph
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.GetEncoding("Cyrillic");
+            Console.InputEncoding = Encoding.GetEncoding("Cyrillic");
+
+            //Test1("08_1"); 
+
+            Test1("08_2"); //Failed
+
+            //Test1("08_3"); //Failed
+
+            //Test1("08_4"); //Failed
+            //Console.Read();
+        }
+
+        private static void Test1(string test)
+        {
+
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("Практическое задание #1 (тест): " + test);
+            Console.WriteLine("--------------------------------------------");
+
+            var inputFileName = @"Data\test_08\test_" + test + ".txt";
+            var inputReader = new StreamReader((new FileInfo(inputFileName)).OpenRead());
+
+            var outputFileName = @"Data\test_08\test_" + test + ".output.txt";
+            var outputReader = new StreamReader((new FileInfo(outputFileName)).OpenRead());
+
+            var graph = new Graph();
+
+            while (!inputReader.EndOfStream)
+            {
+                var row = inputReader.ReadLine();
+                var vs = row.Split(' ');
+
+                if (vs.Length > 1 && vs[0] == vs[1])
+                {
+                    continue;
+                }
+
+                if (!graph._graph.ContainsKey(vs[0]))
+                {
+                    graph._graph.Add(vs[0], new List<string>());
+                }
+
+                if(vs.Length > 1)
+                    graph._graph[vs[0]].Add(vs[1]);
+            }
+            //Validate Graph
+            var length = graph._graph.Count;
+            for (var i=0; i < length; ++i)
+            {
+                var v = graph._graph.Keys.ElementAt(i);
+                foreach (var u in graph._graph[v])
+                {
+                    if (!graph._graph.Keys.Contains(u))
+                    {
+                        graph._graph.Add(u, new List<string>());
+                    }
+                }
+            }
+
+            var outPutResults = new List<string>();
+
+            Console.WriteLine("OUTPUT:");
+            while (!outputReader.EndOfStream)
+            {
+                var row = outputReader.ReadLine();
+                var vs = row.Split(' ');
+
+                outPutResults.Add(row);
+                Console.WriteLine(row);
+            }
+
+            var result = graph.StrongConnectedComponents();
+            Console.WriteLine(new string('.', 50));
+
+            Console.WriteLine("RESULT:");
+            foreach (var r in result)
+            {
+                var sc = String.Join(" ", (r as List<string>));
+                Console.WriteLine("({0}) ", sc);
+            }
+            Console.WriteLine(new string('.', 50));
+
+            foreach (var outPutResult in outPutResults)
+            {
+                var ors = outPutResult.Split(' ');
+
+                foreach (var or in ors)
+                {
+                    Console.Write("{0} -->", or);
+
+                    var count = Convert.ToInt32(or);
+                    var isResultFount = false;
+                    foreach (var r in result)
+                    {
+                        if ((r as List<string>).Count == count)
+                        {
+                            isResultFount = true;
+
+                            var sc = String.Join(" ", (r as List<string>));
+                            Console.Write("({0}) ", sc);
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("Passed");
+                            Console.ResetColor();
+                        }
+                    }
+
+                    if (!isResultFount)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Failed");
+                        Console.ResetColor();
+                    }
+                }
+            }
         }
     }
 
-    public class BreadthFirstSearch
+    public class Graph
     {
+        internal IDictionary<string, IList<string>> _graph;
+        internal IDictionary<string, int> _bfs;
+        internal IDictionary<string, int> _dfs;
+        internal IDictionary<string, int> _ts;
+        internal IDictionary<string, int> _f;
+        internal IList<string> _dfs_t;
+        internal IDictionary<string, int> _d;
+        internal IDictionary<string, string> _p;
+        private int _label;
+        private int _t;
 
+
+        public Graph()
+        {
+            _graph = new Dictionary<string, IList<string>>();
+            _bfs = new Dictionary<string, int>();
+            _d = new Dictionary<string, int>();
+            _p = new Dictionary<string, string>();
+            _f = new Dictionary<string, int>();
+
+             _dfs = new Dictionary<string, int>();
+
+             _ts = new Dictionary<string, int>();
+            _dfs_t = new List<string>();
+        }
+
+        public void BreadthFirstSearch()
+        {
+            _bfs.Clear();
+            _d.Clear();
+            _p.Clear();
+
+            var v = _graph.Keys.First();
+            BFS(v);
+        }
+        private void BFS(string s)
+        {
+            var bfsQueue = new Queue<string>();
+
+            var k = 1;
+
+            _bfs[s] = k;
+            _d[s] = 0;
+
+            bfsQueue.Enqueue(s);
+
+            while (bfsQueue.Count > 0)
+            {
+                var v = bfsQueue.Dequeue();
+                foreach (var u in _graph[v])
+                {
+                    if (!_bfs.ContainsKey(u))
+                    {
+                        k += 1;
+
+                        _bfs[u] = k;
+                        _d[u] = _d[v] + 1;
+                        _p[u] = v;
+
+                        bfsQueue.Enqueue(u);
+                    }
+                }
+
+            }
+
+        }
+        public int FindConnectedComponents()
+        {
+            var N = 0;
+
+            _bfs.Clear();
+           
+
+            foreach (var v in _graph.Keys)
+            {
+                if (!_bfs.ContainsKey(v))
+                {
+                    _d.Clear();
+                    _p.Clear();
+
+                    BFS(v);
+                    N += 1;
+                }
+            }
+
+            return N;
+        }
+
+
+        public void DepthFirstSearch1()
+        {
+            _dfs.Clear();
+
+            var v = _graph.Keys.First();
+            var k = DFSR(v, 1);            
+        }
+        public void DepthFirstSearch2()
+        {
+            _dfs.Clear();
+
+            var v = _graph.Keys.First();
+            DFS(v);
+        }
+        private int DFSR(string s, int k)
+        {
+            _dfs[s] = k;
+            foreach (var u in _graph[s])
+            {
+                if (!_dfs.ContainsKey(u))
+                {
+                    k = DFSR(u, k + 1);
+                }
+            }
+
+            return k;
+        }
+        private void DFS(string s)
+        {
+            var k = 1;
+            var dfsStack = new Stack<string>();
+            
+            _dfs[s] = k;
+            dfsStack.Push(s);
+            while (dfsStack.Count > 0)
+            {
+                var v = dfsStack.Peek();
+                var isNotFoundExist = false;
+
+                foreach (var u in _graph[v])
+                {
+                    if (!_dfs.ContainsKey(u))
+                    {
+                        k += 1;
+                        _dfs[u] = k;
+                        dfsStack.Push(u);
+                        isNotFoundExist = true;
+                        break;
+
+                    }
+                }
+
+                if(!isNotFoundExist)
+                    dfsStack.Pop();
+            }
+
+        }
+
+        
+        public void TopologicalSort()
+        {
+            _ts.Clear();
+
+            _label = _graph.Count;
+            _dfs_t.Clear();
+
+            foreach (var v in _graph.Keys)
+            {
+                if (!_dfs_t.Contains(v))
+                {
+                    DFSR_Topological(v);                   
+                }
+
+            }
+        }
+        private void DFSR_Topological(string s)
+        {
+            _dfs_t.Add(s);
+            foreach (var u in _graph[s])
+            {
+                if (!_dfs_t.Contains(u))
+                {
+                    DFSR_Topological(u);
+                }
+            }
+
+            _ts[s] = _label;
+            _label -= 1;
+        }
+
+        
+        public IList<IList<string>> StrongConnectedComponents()
+        {
+            var strongComponents = new List<IList<string>>();
+            
+            //1
+            DFSLoop(_graph, null);
+
+            //2 - транспонировать графф
+            var traponedGraph = new Dictionary<string, IList<string>>();
+
+            foreach (var v in _graph.Keys)
+            {
+                traponedGraph.Add(v, new List<string>());
+                foreach(var u in _graph.Keys)
+                {
+                    if (_graph[u].Contains(v))
+                    {
+                        traponedGraph[v].Add(u);
+                    }
+                }
+            }
+
+            //3
+            Console.WriteLine("--------->");
+
+            DFSLoop(traponedGraph, strongComponents);
+
+            return strongComponents;
+        }
+        internal void DFSLoop(IDictionary<string, IList<string>> graph, IList<IList<string>> strongComponents)
+        {
+            _dfs_t.Clear();
+            _t = 0;
+            foreach (var v in graph.Keys)
+            {
+                if (!_dfs_t.Contains(v))
+                {
+                    if (strongComponents != null)
+                        strongComponents.Add(new List<string> { v } );
+
+                    Console.Write("{0} --> ", v);
+
+                    DFSR_Topological_StrongComponents(graph, v, strongComponents);
+                }
+            }
+        }
+        internal void DFSR_Topological_StrongComponents(IDictionary<string, IList<string>> graph, string s, IList<IList<string>> strongComponents)
+        {
+            _dfs_t.Add(s);
+
+            foreach (var u in graph[s])
+            {
+                if (!_dfs_t.Contains(u))
+                {
+                    if (strongComponents != null)
+                        strongComponents.Last().Add(u);
+                    Console.Write("{0} --> ", u);
+
+                    DFSR_Topological_StrongComponents(graph, u, strongComponents);
+                }
+            }
+
+          
+            _t += 1;
+            _f[s] = _t;
+            Console.WriteLine();
+        }
     }
 }
