61d552bec0222c601c14f05c8c92cae876bf7616
Valentyn Ponomarenko <bootloader@list.ru>
2015-05-01 13:12:28 -0500

    Задание 9: Реализация алогритма Дейкстры. Результаты тестового и практического задания

--- Dijkstra/Program.cs
+++ Dijkstra/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿#define LOG1
+
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -12,6 +15,302 @@ namespace Dijkstra
             Console.OutputEncoding = Encoding.GetEncoding("Cyrillic");
             Console.InputEncoding = Encoding.GetEncoding("Cyrillic");
 
+            //==============
+            //10 x 10
+            //==============
+            //Test1("5_10", "5");
+            //Test1("6_10", "6");
+            //Test1("7_10", "7");
+            //Test1("8_10", "8");
+
+            //==============
+            //100 x 100
+            //==============
+            //Test1("1_100", "1"); //Отсутствуют вершины - 94
+            //Test1("2_100", "2"); //Отсутствуют вершины - 57, 61
+            //Test1("3_100", "3"); //Отсутствуют вершины - 
+
+            //==============
+            //1000 x 1000
+            //==============
+            //Test1("4_1000", "4"); //Отсутствуют вершины - 
+
+            Task1("USA-FLA");
+
+            Console.Read();
+            Console.Read();
+        }
+
+        private static void Test1(string input, string output)
+        {
+
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("Практическое задание #1 (тест): " + input);
+            Console.WriteLine("--------------------------------------------");
+
+            var inputFileName = @"Data\test_09\input_" + input + ".txt";
+            var inputReader = new StreamReader((new FileInfo(inputFileName)).OpenRead());
+
+            var outputFileName = @"Data\test_09\output_" + output + ".txt";
+            var outputReader = new StreamReader((new FileInfo(outputFileName)).OpenRead());
+
+            var graph = new Graph<string>();
+            var vertexCount = 0;
+            var edgeCount = 0;
+
+            #region INPUT
+
+            var keys = new List<int>();
+            var row = inputReader.ReadLine();
+            var vs = row.Split(' ');
+            vertexCount = Convert.ToInt32(vs[0]);
+            edgeCount = Convert.ToInt32(vs[0]);
+
+            graph._graph = new Dictionary<string, IList<KeyValuePair<string, decimal>>>(vertexCount);
+
+            while (!inputReader.EndOfStream)
+            {
+                row = inputReader.ReadLine();
+                vs = row.Split(' ');
+
+                var key = vs[1];
+                var cost = Convert.ToDecimal(vs[2]);
+
+                if(!keys.Contains(Convert.ToInt32(key))) keys.Add(Convert.ToInt32(key));
+
+                if (!graph._graph.ContainsKey(vs[0]))
+                {
+                    graph._graph.Add(new KeyValuePair<string, IList<KeyValuePair<string, decimal>>>(vs[0], new List<KeyValuePair<string, decimal>>()));
+                }
+
+                graph._graph[vs[0]].Add(new KeyValuePair<string, decimal>(key, cost));
+            }
+
+            //Validate Graph
+            var length = graph._graph.Count;
+            for (var i = 0; i < length; ++i)
+            {
+                var v = graph._graph.Keys.ElementAt(i);
+                foreach (var u in graph._graph[v])
+                {
+                    if (!graph._graph.ContainsKey(u.Key))
+                    {
+                        graph._graph.Add(u.Key, null);
+                    }
+                }
+            }
+
+            #endregion
+
+            #region OUTPUT
+
+            var outPutResults = new int?[vertexCount][];
+
+            Console.WriteLine("OUTPUT:");
+
+            {
+                var i = 0;
+                while (!outputReader.EndOfStream)
+                {
+                    row = outputReader.ReadLine();
+                    vs = row.Split(' ');
+
+                    var vsCount = vs.Length;
+                    outPutResults[i] = new int?[vertexCount];
+
+                    for (var j = 0; j < vsCount; ++j)
+                    {
+                        int shortPath;
+                        if (int.TryParse(vs[j], out shortPath))
+                        {
+                            outPutResults[i][j] = shortPath;
+                        }
+                    }
+
+                    ++i;
+
+                    Console.WriteLine(row);
+                }
+            }
+        
+
+            Console.WriteLine(new string('.', 50));
+            #endregion
+
+            var resultFile = File.AppendText(input + "-result.txt");
+
+            for (var i=0; i< outPutResults.Length; ++i)
+            {
+                var outPutResult = outPutResults[i];
+                for (var j = 0; j < outPutResult.Length; ++j)
+                {
+                    var result = outPutResult[j];
+
+                    if (result == null) continue;
+
+
+                    var s = Convert.ToString(i + 1); //keys[i]);
+                    var e = Convert.ToString(j + 1); //keys[j]);
+
+                    if (!graph._graph.ContainsKey(s))
+                    {
+                        Console.Write("----> Начальная вершина: {0}", s);
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("- НЕ СУЩЕСТВУЕТ.");
+                        Console.ResetColor();
+
+                        resultFile.Write("----> Начальная вершина: {0} - НЕ СУЩЕСТВУЕТ.", s);
+                        continue;
+                    }
+
+                    if (!graph._graph.ContainsKey(e))
+                    {
+                        Console.Write("----> Конечная вершина: {0}", e);
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("- НЕ СУЩЕСТВУЕТ.");
+                        Console.ResetColor();
+
+                        resultFile.Write("----> Конечная вершина: {0} - НЕ СУЩЕСТВУЕТ.", e);
+
+                        continue;
+                    }
+
+                    IDictionary<string, decimal> a;
+                    IDictionary<string, string> b;
+
+                    graph.Dijkstra2(s, e, out a, out b);
+
+                    Console.Write("{0}:{1} --> (Output:{2}, Result:{3}) -- ", s, e, result, a[e]);
+                    if (a[e] == result)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Passed");
+                        Console.ResetColor();
+                        continue;
+                    }
+
+                    resultFile.Write("{0}:{1} --> (Output:{2}, Result:{3}) -- Failed", s, e, result, a[e]);
+
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Failed");
+                    Console.ResetColor();
+                }
+            }
+        }
+
+        private static void Task1(string input)
+        {
+            Console.WriteLine();
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("Практическое задание #1: " + input);
+            Console.WriteLine("--------------------------------------------");
+            Console.WriteLine("ATTENTION: Выполнение задания занимает значительное время. Ориентировочно 48 часов.");
+            Console.WriteLine("--------------------------------------------");
+
+            var inputFileName = @"Data\" + input + ".txt";
+            var inputReader = new StreamReader((new FileInfo(inputFileName)).OpenRead());
+
+            var graph = new Graph<string>();
+            var vertexCount = 0;
+            var edgeCount = 0;
+
+            #region INPUT
+
+            var row = inputReader.ReadLine();
+            var vs = row.Split(' ');
+            vertexCount = Convert.ToInt32(vs[0]);
+            edgeCount = Convert.ToInt32(vs[0]);
+
+            graph._graph = new Dictionary<string, IList<KeyValuePair<string, decimal>>>(vertexCount);
+
+            while (!inputReader.EndOfStream)
+            {
+                row = inputReader.ReadLine();
+                vs = row.Split(' ');
+
+                var key = vs[1];
+                var cost = Convert.ToDecimal(vs[2]);
+
+                if (!graph._graph.ContainsKey(vs[0]))
+                {
+                    graph._graph.Add(new KeyValuePair<string, IList<KeyValuePair<string, decimal>>>(vs[0], new List<KeyValuePair<string, decimal>>()));
+                }
+
+                graph._graph[vs[0]].Add(new KeyValuePair<string, decimal>(key, cost));
+            }
+
+            //Validate Graph
+            var length = graph._graph.Count;
+            for (var i = 0; i < length; ++i)
+            {
+                var v = graph._graph.Keys.ElementAt(i);
+                foreach (var u in graph._graph[v])
+                {
+                    if (!graph._graph.ContainsKey(u.Key))
+                    {
+                        graph._graph.Add(u.Key, null);
+                    }
+                }
+            }
+
+            #endregion
+            Console.WriteLine("Файл прочитан:{0}", DateTime.Now);
+
+            var resultFile = File.AppendText(input + "-result.txt");
+
+            var s = "100562";
+            var e = "1070345";
+            IDictionary<string, decimal> a;
+            IDictionary<string, IList<string>> b;
+
+            graph.Dijkstra3(s, e, out a, out b);
+
+            Console.WriteLine("Путь расчитан:{0}", DateTime.Now);
+
+            Console.WriteLine(new string('.', 50));
+            var question1 =
+                String.Format("Вопрос 1: Наиболее короткое расcтояние между вершинами: {0} --> {1} равно: {2}", s, e,
+                    a[e]);
+            Console.WriteLine(question1);
+
+            resultFile.Write(question1);
+            resultFile.Write(Environment.NewLine);
+            
+            Console.WriteLine(new string('.', 50));
+
+            var pathCount = getShortestPath(s, e, b);
+            var question2 =
+                String.Format("Вопрос 2: Количество уникальных коротких путей между вершинами: {0} --> {1} равно: {2}", s, e,
+                    pathCount.Length);
+            Console.WriteLine(question2);
+
+            resultFile.Write(question2);
+            resultFile.Write(Environment.NewLine);
+            resultFile.Flush();
+            resultFile.Dispose();
+            resultFile.Close();
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+        }
+        private static string[] getShortestPath(string s, string e, IDictionary<string, IList<string>> b)
+        {
+            var pathes = new List<string>();
+
+            if (!b.ContainsKey(e))
+            {
+                return new[] { e };
+            }
+
+            foreach (var v in b[e])
+            {
+                var pp = getShortestPath(s, v, b);
+                pathes.AddRange(pp.Select(p => e + " " + p));
+            }
+
+            return pathes.ToArray();
         }
     }
 
@@ -27,26 +326,26 @@ namespace Dijkstra
         //Алгоритм с использование циклов. Время выполенения Q(n^2)
         public void Dijkstra1(T s, T e, out IDictionary<T, decimal> a, out IDictionary<T, T> b)
         {
-            var _a = _graph.Keys.ToDictionary(g => g, g => decimal.MaxValue);
-            var _b = new Dictionary<T, T>();
+            a = _graph.Keys.ToDictionary(g => g, g => decimal.MaxValue);
+            b = new Dictionary<T, T>();
             var _x = new List<T>();
 
-            _a[s] = 0;
+            a[s] = 0;
             _x.Add(s);
             var v = s;
-            while (_x.Count < _a.Count)
+            while (_x.Count < a.Count)
             {
                 foreach (var u in _graph[v])
                 {
-                    if (_a[u.Key] > _a[v] +  u.Value)
+                    if (a[u.Key] > a[v] +  u.Value)
                     {
-                        _a[u.Key] = _a[v] + u.Value;
-                        _b[u.Key] = v;
+                        a[u.Key] = a[v] + u.Value;
+                        b[u.Key] = v;
                     }
                 }
 
                 var minValue = decimal.MaxValue;
-                foreach (var u in _a)
+                foreach (var u in a)
                 {
                     if (u.Value < minValue &&  !_x.Contains(u.Key))
                     {
@@ -59,9 +358,6 @@ namespace Dijkstra
                 if (v == e)
                      break;
             }
-
-            a = _a;
-            b = _b;
         }
 
         //Алгоритм с использование приоритезированной очереди. Время выполенения Q(n*log n)
@@ -77,20 +373,105 @@ namespace Dijkstra
 
             while (queue.Size > 0)
             {
+#if LOG               
+                Console.WriteLine("-------");  
+#endif
                 var v = queue.Dequeue();
-  
+#if LOG
+                Console.WriteLine("var v = queue.Dequeue(): -- >v = {0}", v);
+#endif                
+                if(_graph[v] == null) continue;
+
+                var vValue = _graph[v];
+                var vCount = _graph[v].Count;
+
+                for (var i = 0; i < vCount; ++i)
+                {
+                    
+                    var u = vValue[i];
+#if LOG
+                    Console.WriteLine("          var u = vValue[i];  --> u = {0}", u);
+
+                    Console.WriteLine("          a[v]:{0} < decimal.MaxValue && a[u.Key]:({1}) > a[v]:({0}) + u.Value:({2});", a[v], a[u.Key], u.Value);
+#endif 
+                    if (a[v] < decimal.MaxValue && a[u.Key] >  a[v] + u.Value)
+                    {
+                        a[u.Key] = a[v] + u.Value;
+
+                        if (b.ContainsKey(u.Key))
+                        {
+                            var err = 1;
+                        }
+
+                        b[u.Key] = v;
+                        queue.DecreaseKey(u.Key, a[u.Key]);
+#if LOG                        
+                        Console.WriteLine("          a[u.Key]:{0} = a[v]:({1}) + u.Value:({2})", u.Key, a[v], u.Value);
+                        Console.WriteLine("          b[u.Key]:({0}) = v;:({1})", u.Key, v);
+                        Console.WriteLine("          queue.DecreaseKey(u.Key:({0}), a[u.Key];({1}))", u.Key, a[u.Key]);
+                        Console.WriteLine();
+#endif 
+
+                    }
+                }
+            }
+        }
+
+        //Алгоритм с использование приоритезированной очереди и нахождением нескольких . 
+        //Время выполенения Q(n*log n)
+        public void Dijkstra3(T s, T e, out IDictionary<T, decimal> a, out IDictionary<T, IList<T>> b)
+        {
+            var queue = new Queue<T>(QueueType.LowToHigh);
+            a = _graph.Keys.ToDictionary(g => g, g => decimal.MaxValue);
+            b = new Dictionary<T, IList<T>>();
+
+            a[s] = 0;
+
+            a.ToList().ForEach(v => queue.Enqueue(v.Key, v.Value));
+
+            while (queue.Size > 0)
+            {
+#if LOG               
+                Console.WriteLine("-------");  
+#endif
+                var v = queue.Dequeue();
+#if LOG
+                Console.WriteLine("var v = queue.Dequeue(): -- >v = {0}", v);
+#endif
+                if (_graph[v] == null) continue;
+
                 var vValue = _graph[v];
                 var vCount = _graph[v].Count;
 
                 for (var i = 0; i < vCount; ++i)
                 {
                     var u = vValue[i];
+#if LOG
+                    Console.WriteLine("          var u = vValue[i];  --> u = {0}", u);
+
+                    Console.WriteLine("          if(a[v]:{0} < decimal.MaxValue && a[u.Key]:({1}) > a[v]:({0}) + u.Value:({2}))", a[v], a[u.Key], u.Value);
+#endif
                     if (a[v] < decimal.MaxValue && a[u.Key] > a[v] + u.Value)
                     {
                         a[u.Key] = a[v] + u.Value;
-                        b[u.Key] = v;
-
+                        b[u.Key] = new List<T> {v};
                         queue.DecreaseKey(u.Key, a[u.Key]);
+#if LOG                        
+                        Console.WriteLine("          a[u.Key]:{0} = a[v]:({1}) + u.Value:({2})", u.Key, a[v], u.Value);
+                        Console.WriteLine("          b[u.Key]:({0}) = new List<T> {{v:{1}}}; ", u.Key, v);
+                        Console.WriteLine("          queue.DecreaseKey(u.Key:({0}), a[u.Key];({1}))", u.Key, a[u.Key]);
+                        Console.WriteLine();
+#endif
+                    }
+                    else if (a[v] < decimal.MaxValue && a[u.Key] == a[v] + u.Value)
+                    {
+                        if (!b[u.Key].Contains(v)) b[u.Key].Add(v);
+
+#if LOG
+                        Console.WriteLine("          else if (a[v]:{0} < decimal.MaxValue && a[u.Key]:({1}) == a[v]:({0}) + u.Value:({2}))", a[v], a[u.Key], u.Value);
+                        Console.WriteLine("          b[u.Key::({0})].Add(v::({1});", u.Key, v);
+                        Console.WriteLine();
+#endif
                     }
                 }
             }
@@ -103,7 +484,6 @@ namespace Dijkstra
         LowToHigh = 2
     }
 
-   
     //Приоритезированная очередь на основе Пирамид
     public class Queue<T> where T: class 
     {
@@ -323,11 +703,8 @@ namespace Dijkstra
             var i = 0;
             while (qi == null && i < Size)
             {
-                if (_array[i].Key == key)
+                if (EqualityComparer<T>.Default.Equals(_array[i].Key, key))
                 {
-                    qi = _array[i];
-                    qi.Value = value;
-
                     HeapDecreseKey(i, value);
                 }
                 ++i;
---
+++ Dijkstra/Tests/WhenIExecute_Dijkstra3_on_graph_with_more_than_one_shortest_route.cs
@@ -0,0 +1,236 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+using key = System.Collections.Generic.KeyValuePair<string, System.Collections.Generic.IList<System.Collections.Generic.KeyValuePair<string, decimal>>>;
+using vertex = System.Collections.Generic.KeyValuePair<string, decimal>;
+
+namespace Dijkstra.Tests
+{
+    public class WhenIExecute_Dijkstra3_on_graph_with_more_than_one_shortest_route
+    {
+        private readonly Graph<string> _graph;
+
+        public WhenIExecute_Dijkstra3_on_graph_with_more_than_one_shortest_route()
+        {
+            _graph = new Graph<string>();
+        }
+
+        /*
+         1 2 1
+         1 6 8
+         1 8 7
+         
+         2 8 8
+         2 9 4
+         
+         3 9 6
+         3 10 2
+         
+         4 10 2
+         
+         5 6 3
+         5 7 1
+         5 10 5
+         
+         6 5 2
+         7 10 1
+         
+         8 4 3
+         8 9 12
+         8 10 5
+         
+         9 1 5
+         9 3 5
+         9 5 11
+         9 6 10
+         9 8 12          
+         */
+        private void generateGraph1()
+        {
+            _graph._graph.Add(new key("1", new List<vertex>
+            {
+                new vertex("2", 1),
+                new vertex("6", 8),
+                new vertex("8", 7),
+            }));
+
+            _graph._graph.Add(new key("2", new List<vertex>
+            {
+                new vertex("8", 8),
+                new vertex("9", 4)
+            }));
+
+            _graph._graph.Add(new key("3", new List<vertex>
+            {
+                new vertex("9", 6),
+                new vertex("10", 2)
+            }));
+
+            _graph._graph.Add(new key("4", new List<vertex>
+            {
+                new vertex("10", 2)
+            }));
+
+            _graph._graph.Add(new key("5", new List<vertex>
+            {
+                new vertex("6", 3),
+                new vertex("7", 1),
+                new vertex("10",5)
+            }));
+
+            _graph._graph.Add(new key("6", new List<vertex>
+            {
+                new vertex("5", 2)
+            }));
+
+            _graph._graph.Add(new key("7", new List<vertex>
+            {
+                new vertex("10", 1)
+            }));
+
+            _graph._graph.Add(new key("8", new List<vertex>
+            {
+                new vertex("4", 3),
+                new vertex("9", 12),
+                new vertex("10",5)
+            }));
+
+            _graph._graph.Add(new key("9", new List<vertex>
+            {
+                new vertex("1", 5),
+                new vertex("3", 5),
+                new vertex("5", 11),
+                new vertex("6", 10),
+                new vertex("8", 12)
+            }));
+
+            _graph._graph.Add(new key("10", null));
+        }
+
+        /*
+            7 10
+            1 2 1
+            1 3 3
+            1 7 14
+            1 7 14
+         
+            2 4 7
+            
+            3 4 5
+            
+            4 5 3
+            4 6 2
+            
+            5 7 3
+            
+            6 7 4
+         */
+        private void generateGraph2()
+        {
+            _graph._graph.Add(new key("1", new List<vertex>
+            {
+                new vertex("2", 1),
+                new vertex("3", 3),
+                new vertex("7", 14),
+                new vertex("7", 14)
+            }));
+
+            _graph._graph.Add(new key("2", new List<vertex>
+            {
+                new vertex("4", 7)
+            }));
+
+            _graph._graph.Add(new key("3", new List<vertex>
+            {
+                new vertex("4", 5)
+            }));
+
+            _graph._graph.Add(new key("4", new List<vertex>
+            {
+                new vertex("5", 3),
+                new vertex("6", 2)
+            }));
+
+            _graph._graph.Add(new key("5", new List<vertex>
+            {
+                new vertex("7", 3)
+            }));
+
+            _graph._graph.Add(new key("6", new List<vertex>
+            {
+                new vertex("7", 4)
+            }));
+
+            _graph._graph.Add(new key("7", null));
+        }
+
+        [Fact]
+        public void Should_find_all_shortes_routes_12()
+        {
+            //Stub
+            generateGraph1();
+
+            IDictionary<string, decimal> _a;
+            IDictionary<string, IList<string>> _b;
+
+            //
+            _graph.Dijkstra3("1", "10", out _a, out _b);
+
+            _a["10"].Should().Be(12);
+
+            var result = getShortestPath("1", "10", _b);
+
+
+            result.Length.Should().Be(4);
+
+            result.Contains("10 4 8 1");
+            result.Contains("10 3 9 2 1");
+            result.Contains("10 8 1");
+            result.Contains("10 7 5 6 1");
+        }
+
+        [Fact]
+        public void Should_find_all_shortes_routes_14()
+        {
+            //Stub
+            generateGraph2();
+
+            IDictionary<string, decimal> _a;
+            IDictionary<string,  IList<string>> _b;
+
+            //
+            _graph.Dijkstra3("1", "7", out _a, out _b);
+
+            _a["7"].Should().Be(14);
+
+            var result = getShortestPath("1", "7", _b);
+
+            result.Length.Should().Be(5);
+
+            result.Contains("7 1");
+            result.Contains("7 6 4 3 1");
+            result.Contains("7 6 4 2 1");
+            result.Contains("7 5 4 3 1");
+            result.Contains("7 5 4 2 1");
+        }
+
+        private string[] getShortestPath(string s, string e, IDictionary<string, IList<string>> b)
+        {
+            var pathes = new List<string>();
+
+            if (!b.ContainsKey(e))
+            {
+                return new[] {e};
+            }
+
+            foreach (var v in b[e])
+            {
+                var pp = getShortestPath(s, v, b);
+                pathes.AddRange(pp.Select(p => e + " " + p));
+            }
+
+            return pathes.ToArray();
+        }
+    }
+}
